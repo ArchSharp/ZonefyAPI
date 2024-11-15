134f5895a6488924c00a5e66532780a023dce3d2
raphael olaiyapo <archraphr@gmail.com>
2024-11-15 18:17:26 +0100

    g

--- Configurations/GoogleDriveAuthConfig.cs
+++ Configurations/GoogleDriveAuthConfig.cs
@@ -36,17 +36,17 @@ namespace ZonefyDotnet.Configurations
             // Reconstructed service account object
             var serviceAccount = new
             {
-                type = "service_account",
-                project_id = "zoneefystorage",
-                private_key_id = "fcfc46be0a9138502e5eae7ea5a6b8b358b28ac2",
-                private_key = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC/62aRICiBb89w\nTAV4Exp7VGIkCHTsBFtf8t4FtHny0T5ZxCWjZbFNiLYCD8ntWHZ3Xb1YbRT8PVz4\no7NMTjww1IRZW2mHc4L11rECgzEnrGmCypKDSYFNyJHg+2zIK83DFEw7mqcWzXie\nEfNmQLatDafL/UdD739K7F36Qq2tJKGEGdmZnGIYy/x0hupMD1GEtVOEfXVqmkdp\nZTdHugYT/04RhRWAGxRjDa3cVhLLM2CKPsNa0KegQZ49HytX1mWKggQ5Ij7bhjfy\nJvrPfrhsfmMvDP1eLXqhb6tzlOXO6dR18LVLX+CIYSTblW8V2mvL6DhUgMttxiqJ\n7lqW1mmtAgMBAAECggEAWvkBTfKh8LUo7fDDOOdJSsO1n6H4lnX8QE+WCbtCgSTJ\nevCc7MbEFF4k9HY6gAzKXwuquNNAOs71lpC2aYm+Q2FBeT/5FnFdVx4VGv1IHCfA\nffR7HcuGhPH5oF1d88a7yuiAuFI3OPpd3gPhTguH7CEIdIwTL3ND8iZ356yaufLL\nsiPuZoZBB64QTkfUeW5dpRAHVde7w2dhZyL2uiwxaPYsTyxRGNqYXlNmL5REjDdf\nlcJWXIFRvI7j3HeGwSmNIaruWsAufKpQlytFxqtYTGms7O84AqlB019dTpX3gWk/\nSeT9ZGLbOM1q+F2elHAmv5n8fQjRJV1VlBcoppX6uwKBgQDysKK1m4ppNhFW1kRI\ny9JxhvFs9i6DCKFl3tDLW1FF+xgqgf/X7OTYH8e96zdfuGElR0upKCx3YjS5Mmk6\ng9oECQNYm4aw5ijWZ+TGvxWbrid6MEpyjiSSgWc4id9RPHEr1EadPxIBWA9Nmpmw\nWCM5GxHHYl5NK30Wo4wCviocFwKBgQDKcfLWKfqwE29IPIVZZa3yziY5jG7duOXE\ntyNZPJZXUwgh2ygA6Pqe5VPZ/SwZBuoEAcEcAxZRyoT9kkIM9LtQe6OpmF7StRXP\nEAawmOp/wLmyQGhsQ/l1scFJBaBaCBRwORrPtFMumkTDyX5AtDVHSURnWoIh+oWb\nFZfII8Xu2wKBgBdGI0SBVyPB4KXQRJoqmdLzWwHVFh7Q3BGV7bYi2sd4Thf9gZim\nikfDvHcVMKlIWGCn1rSxOpp0W5QLJpZLAMR0TNLZj3A1kmMFcaYxuxPD/tsz+F/G\nC/HU4R5F8pmqzzuhzufTnZltZYJOukB0I2SdmPXjFCIYcpWSN6IntIgfAoGBALzJ\n3qeRplIlDZXTupdPlxPi4wqvx4PFNaBpGPHS6nCs4dyQv5F5sp4TRkr3KfR82ia6\nbBdvWZUUw0ik6Cuk3jmD03AIxATDEn9ydoqhT62rUIDGR5sPGMeE6LbsJElRV53/\n4tznUNsYyh+6+53jb/v2e2/Wm5yMP6QMUus1URQZAoGBAImFSJL+ed46/UwkXICt\nE28IrYgVwqIe+bz8WCPH0vLFLp7ssObpjE3x5LJZEtKvYRPNfObD85dx0w/D9XxL\n9p4dWGEmpc3jKHhs4EjN3I4PD7pcPhUaTK+osH/7dPo3e6UGzO2pgOZa2QZ40Fd+\np35s2tCqC/FggIQMLG7xJORi\n-----END PRIVATE KEY-----\n",
-                client_email = "zonefystorage@zoneefystorage.iam.gserviceaccount.com",
-                client_id = "116471458008477900798",
-                auth_uri = "https://accounts.google.com/o/oauth2/auth",
-                token_uri = "https://oauth2.googleapis.com/token",
-                auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs",
-                client_x509_cert_url = "https://www.googleapis.com/robot/v1/metadata/x509/zonefystorage%40zoneefystorage.iam.gserviceaccount.com",
-                universe_domain = "googleapis.com"
+                  type= "service_account",
+                  project_id= "zoneefystorage",
+                  private_key_id= "a95dfddf7c15eba2de0354a240404b4d3f1721c6",
+                  private_key= "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCx7yLo53T4ffoU\nKYHTjTbvnv6ksuaPVyvsfIlNMlwY7eMpM0xhPgwgb0OBaSd0XsrSD/hCYkKPg9/O\nlcrMTJ0JmeFS2E4g+sbXt5UTpqvQ5Gqv+5jBItekOoO2lt74+dSpT7rGsCyfKbXJ\n3tkz/wKJsX1vDumNoch/KvEuXMMPUIK8gs8zaF26OyCLEH/SSZ9wxAEosl/j2sN6\nICwSYO2CHeRYliZtm7/vAvp9Trvllr7TjVoBeHHDSDn8mTmqsc+sDFA5bIvmPNWz\n3n0tQ4kCBNSnGeE8dLNfhgvOdsJK9wg/5v8VAOVixRMIO2lFJiGUVIYikt8y7ybH\niKrjmfK/AgMBAAECggEAT1wnJ9XD1+joFwIEQ4SiUfGKBY89QgVRM+K/okypwvym\n6oNUt7Ji2jyF0TxDOqXktBgnVARbR4M72Pn6P3CUQen+E2MiRZdUBHZ+6QrXaw3V\nf7Qph0qw/zCR+mgD/uv+q51B4shEvqBt0iGwon5EdYFSPqxaAq97qyxN8a2x2iwC\nE7vdS0K0WR4HMSZkGW2z/yINjKC3GH/VvMCHgnSyFtZJGjVB17JODNB5Gt6HFc38\nIYAFeZbqt15K5d3rrEvt9ZP0nKumY75394IKKfxzOz32w6I8mIVD3VmZ/ODSZnRa\ndbkWLWZj2Ijjz7DsvtmHiRe6sMXfCkLbYzsltP2MUQKBgQDah/+cO4XFgnZMJafG\nOZ0TDvG+HXwEe6H52i26u/K8Ro7QK4NdvF4Eq840vKXiCkTA7iuUPe0rwGYcY4Ar\nhCIPq/sipPIG486QFQXqTn9UR8WBYbdXj6sQECceEj1v3QTl0aJw1x9dQaC7d8U8\nMbjbHskpGLgnGkMPPVwxQ2r5CwKBgQDQcTTx660jcEXi/o6r+GbtInJ1pGy2YDns\nkIfIszZUBiYIzb8trsUiY250RdJCg26PtK93T90Aaqz8iyFYMJ6MirV/D2NIouO9\nC+bwne7z1R/UvLbgZCqfPaqjwopJ1dh7kfzGYIXtqb6oPTNQDszXLEfFJpN7b7qZ\nNjayRaYFnQKBgQCl4Glj28KUw4ysDEEns//IwaU53AORG489qiDgB14fD6fD1+Ol\nOSBch1TErxVzWLwLfj3SDpeCiE556gUWAvwfzTmZeH7GQBFBSNPuoJsCDGCc7uFO\nM51zWe93Yf0Edk4LbG7THFeQYApzglxSbqOUn++tpDfUZpjUbo0WT8hZNQKBgQCI\nhV0UBjREPcjIP8naQLvJHQKyoprBaI0HsDH+9cHjjpNiuL86gHmFaHJznTd1cPf9\nD27NSnWJTFU4ozeXt2Bg1IjDS6TPckCPFTiQ6CAVe8V5ZJCyr2hrG8yjrUs9yN4d\nMSTKGXYej0rcjhkJmmf3lnz7V6TI+AjTSHrSkGOI0QKBgQCrGy7H5/fhzctB6m88\nEvTO0BJCZc61hWZh0CYicA41EO3kEBcOM0v+QYhgA1tl5+HbbPO9BHHIiUHwXJcl\nshx469IcgVfSs13uu+w9OrMLSxzOKGx99e8Tz3JB7SWqF2g56Sx/CMMdx/eFqpc0\nRlssK7VkT6PLgwzN6/yr4XFOKg==\n-----END PRIVATE KEY-----\n",
+                  client_email= "zonefystorage@zoneefystorage.iam.gserviceaccount.com",
+                  client_id= "116471458008477900798",
+                  auth_uri= "https://accounts.google.com/o/oauth2/auth",
+                  token_uri= "https://oauth2.googleapis.com/token",
+                  auth_provider_x509_cert_url= "https://www.googleapis.com/oauth2/v1/certs",
+                  client_x509_cert_url= "https://www.googleapis.com/robot/v1/metadata/x509/zonefystorage%40zoneefystorage.iam.gserviceaccount.com",
+                  universe_domain= "googleapis.com"
             };
 
             // Serialize to JSON
