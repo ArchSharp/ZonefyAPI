c7db9f8397b7333c380ed3c9dbb555dcac549000
raphael olaiyapo <archraphr@gmail.com>
2024-12-11 09:44:09 +0100

    changed to aws db settings

---
+++ Migrations/20241211083845_movetoaws.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZonefyDotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class movetoaws : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
